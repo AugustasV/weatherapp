4c95ad229708536d2cd9aac1686123cfcb21f1e2
Augustas <reg1nt1z@gmail.com>
2019-05-24 18:52:06 +0300

    Added additional description from weather api

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,10 +15,11 @@ const app = new Koa();
 
 app.use(cors());
 
+//fetch weather, get json file from API call
 const fetchWeather = async () => {
     const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
     const response = await fetch(endpoint);
-  
+
   return response ? response.json() : {}
 };
 
@@ -26,6 +27,7 @@ router.get('/api/weather', async ctx => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
+  // getting all tables of json file through API
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
 });
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -19,24 +19,42 @@ class Weather extends React.Component {
     super(props);
 
     this.state = {
-      icon: "",
-    };
-  }
+     icon: "",
+ 
+ 
+     description: "",
+  
+    
+     
+    }
+  };
 
   async componentWillMount() {
     const weather = await getWeatherFromApi();
     this.setState({icon: weather.icon.slice(0, -1)});
+    this.setState({description: weather.description});
   }
 
   render() {
     const { icon } = this.state;
-
+    const { description } = this.state;
+    const { wind } = this.state;
     return (
+      
+      <div>
       <div className="icon">
         { icon && <img src={`/img/${icon}.svg`} /> }
+        </div>
+        
+          <div className="description">
+          {description}
+          </div>
       </div>
+
+       
     );
   }
+  
 }
 
 ReactDOM.render(
